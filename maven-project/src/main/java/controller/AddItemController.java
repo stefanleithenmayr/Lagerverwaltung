202a47358dadc1d6a80c2044a2ac48e46b320f8b
ReneDeicker <rene.deicker1105@gmail.com>
2018-02-08 21:32:42 +0100

    addItem

--- maven-project/src/main/java/controller/AddItemController.java
+++ maven-project/src/main/java/controller/AddItemController.java
@@ -64,5 +64,6 @@ public class AddItemController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         quantity = 0;
+        //DBConnection.getInstance().createSequence();
     }
 }
--- maven-project/src/main/resources/db/startScript.sql
+++ maven-project/src/main/resources/db/startScript.sql
@@ -4,6 +4,6 @@ insert into users values('stefanleithenmayr', '12345');
 insert into users values('renedeicker', '12345');
 insert into users values('stuetz', '12345');
 
-CREATE TABLE Items(productID int, productName varchar(255),description VARCHAR(255));
+CREATE TABLE Items(productID int, productName varchar(255),description VARCHAR(255), quantity int);
 
-create Sequence itemID start with 1 INCREMENT BY 1;
+--create SEQUENCE itemID start with 1 INCREMENT BY 1;
